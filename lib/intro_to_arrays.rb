2b982817612b36af50edfc173b50600a2862c71b
William Bainton <baintonw@gmail.com>
2019-05-02 18:03:49 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,26 @@
+def instantiate_new_array
+  array = []
+  return array
+end
+
+def array_with_two_elements
+  array = [1, 2]
+  return array
+end
+
+def first_element
+  array = [1, 2]
+  return array[0]
+end
+
+def third_element
+end
+
+def last_element
+end
+
+def first_element_with_array_methods
+end
+
+def length_of_array
+end
